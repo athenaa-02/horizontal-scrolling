d6b9d79a6aff592647a2fae7f45d1b2fa6a96feb
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-03 22:48:31 +0400

    working on responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+let scrollContainer = document.querySelector('.container');
+let backBtn = document.querySelector('#backBtn');
+let nextBtn = document.querySelector('#nextBtn');
+
+scrollContainer.addEventListener('wheel', (evt) =>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = 'auto'
+})
+
+nextBtn.addEventListener('click', ()=>{
+    scrollContainer.style.scrollBehavior = 'smooth'
+    scrollContainer.scrollLeft += 1250;
+})
+backBtn.addEventListener('click', ()=>{
+    scrollContainer.style.scrollBehavior = 'smooth'
+    scrollContainer.scrollLeft -= 1250;
+})
